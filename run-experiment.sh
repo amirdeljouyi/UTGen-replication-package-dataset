dc85f02697e5c2207b23bc2535553633962853b3
amirdeljouyi <amirdeljouyi@gmail.com>
2025-01-05 19:42:06 +0100

    update utestgen; adding llm_graphql_endpoint

--- run-experiment.sh
+++ run-experiment.sh
@@ -1,2 +1,2 @@
-./run-utestgen.sh SF110-binary 8 true
-./run-utestgen.sh extended-dynamosa-repos-binary 8 true
+./run-utestgen.docker.sh SF110-binary 8 true
+./run-utestgen.docker.sh extended-dynamosa-repos-binary 8 true
